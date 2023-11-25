8458f2bd5c032d9409ad4f23e5e47dc8a6cc0e0e
Petech <petekshamitbeyli@gmail.com>
2023-11-26 00:10:13 +0300

    Super-Quality-Section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,44 @@
+import { shoe8 } from "../assets/images";
+import { Button } from "../components";
+
 const SuperQality = () => {
-  return <div>SuperQality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red">Super </span>
+          <span className="text-coral-red">Quality </span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Step into a realm of superior comfort and style with our intricately
+          crafted footwear. Engineered to elevate your every step, our designs
+          promise unparalleled quality, innovation, and a dash of elegance.
+          Immerse yourself in an experience where each detail is meticulously
+          considered, ensuring not just footwear, but a statement of refined
+          taste and unmatched sophistication.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQality;
